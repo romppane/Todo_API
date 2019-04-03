8731973f829b69d8eb6ace5b3bb498d8c405a8ff
Roni Metsämuuronen <roni.metsamuuronen@hotmail.com>
2019-04-03 10:17:46 +0300

    change listen message

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use('/todo', todo);
 app.use('/priorities', priorities);
 
 
-app.listen(port, () => console.log('Example app listening on port ${port}'));
+app.listen(port, () => console.log('App listening on port %s', port));
